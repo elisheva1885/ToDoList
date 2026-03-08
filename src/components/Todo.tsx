5923c2d4203d4926e429511ea8073b6c120f8797
elisheva1885 <elisheva1885@gmail.com>
2026-03-08 10:24:20 +0200

    fixing small staff the UI

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,6 +1,8 @@
 //change to get the info from the context
+import { TiDelete } from "react-icons/ti";
 
 import './todo.css'
+import { FaTimes } from "react-icons/fa";
 export const ToDo = ()=> {
     const todo = {
         id: 1,
@@ -14,7 +16,9 @@ export const ToDo = ()=> {
         <div className="todo-box">
            <p className={todo.status==='pending'? 'description pending': 'description completed'}>{todo.description}  </p> 
             <button className={todo.status==='pending'? 'button-pending': 'button-completed'} disabled = {todo.status!=='pending'} onClick={showClick}>{todo.status!=='pending'? 'Completed': 'Complete'}</button>
-            <button className="delete-button" onClick={showClick}>X</button>
+            <button className="delete-button" onClick={showClick}>    <FaTimes />
+
+</button>
         </div>
     )
 }
