7fe02d73df672da187b2d19a26cd8ecab4a19c27
elisheva1885 <elisheva1885@gmail.com>
2026-03-08 12:39:40 +0200

    split ToDosContext to seprate files

--- src/store/toDos-context.tsx
+++ src/store/toDos-context.tsx
@@ -1,60 +1,16 @@
-import { createContext, useContext, useReducer, type ReactNode } from "react";
+import {  useReducer } from "react";
+import { ActionType, type Action, type ToDo, type ToDoContext, type ToDosContextProviderProps, type ToDosState } from "./types";
+import { ToDosContext } from "./use-todos-context";
 
-type ToDo = {
-    id: number,
-    description: string,
-    status: 'pending' | 'complete'
-};
 
-type ToDosState = {
-    todos: ToDo[]
-};
-
-type ToDoContext = ToDosState & {
-    addToDo: (description: string) => void,
-    deleteToDo: (id: number) => void,
-    updateToDoStatus: (id: number) => void
-};
-const ToDosContext = createContext<ToDoContext | null>(null);
 
 const initialState : ToDosState = {
     todos: []
 }
 
-type ToDosContextProviderProps = {
-    children: ReactNode
-}
-
-const ActionType =  {
-    ADD  : 'ADD_TO_DO',
-    DELETE : 'DELETE_TO_DO' ,
-    UPDATE  : 'UPDATE_TO_DO'
-} as const
-
-
-
-
-type Action = AddToDoAction | DeleteToDoAction | UpdateToDoAction
-
-type AddToDoAction = {
-    type: typeof ActionType.ADD,
-    payload: string
-}
-
-type DeleteToDoAction = {
-    type: typeof ActionType.DELETE,
-    payload: number
-}
-
-type UpdateToDoAction = {
-    type: typeof ActionType.UPDATE,
-    payload: number
-}
-
-
 const toDosReducer = (state: ToDosState, action: Action): ToDosState => {
     switch (action.type) {
-        case ActionType.ADD:
+        case ActionType.ADD:{
             const todo : ToDo = {
                 id: (state.todos[state.todos.length - 1]?.id ?? 0 )+1,
                 description : action.payload,
@@ -65,6 +21,7 @@ const toDosReducer = (state: ToDosState, action: Action): ToDosState => {
                 todos: [  ...state.todos,todo]
 
             }
+        }
         case ActionType.DELETE:
             return {
                 ...state,
@@ -84,13 +41,7 @@ const toDosReducer = (state: ToDosState, action: Action): ToDosState => {
     }
 }
 
-export const useToDosContext= ()=>{
-    const todosCtx  = useContext(ToDosContext)
-    if(todosCtx=== null){
-        throw new Error('ToDosContext is null - error')
-    }
-    return todosCtx;
-}
+
 
 export const ToDosProvider = ({ children }: ToDosContextProviderProps) => {
     const [todosState, dispatch] = useReducer(toDosReducer, initialState)
---
+++ src/store/use-todos-context.tsx
@@ -0,0 +1,13 @@
+import { createContext, useContext } from "react";
+import type { ToDoContext } from "./types";
+
+
+export const ToDosContext = createContext<ToDoContext | null>(null);
+
+export const useToDosContext= ()=>{
+    const todosCtx  = useContext(ToDosContext)
+    if(todosCtx=== null){
+        throw new Error('ToDosContext is null - error')
+    }
+    return todosCtx;
+}
