c1e10ff25272aaf2931fdfc5192a8ff5b2e124fb
elisheva1885 <elisheva1885@gmail.com>
2026-03-09 14:45:13 +0200

    add checks for the input

--- src/components/add-todo.tsx
+++ src/components/add-todo.tsx
@@ -4,23 +4,20 @@ import './add-todo.css'
 import { useToDosContext } from '../store/use-todos-context';
 export const AddToDo = () => {
     const ctx = useToDosContext();
-    const input_ref = useRef(null);
-    // const todo = {
-    //     id: 1,
-    //     desc: '',
-    //     status : 'pending'
-    // }
+    const input_ref = useRef<HTMLInputElement>(null);
     const save = () => {
-        const desc = input_ref.current?.value;
-        ctx.addToDo(desc)
-        
+        const input = input_ref.current;
+        if (input === null || input.value === '') {
+            return;
+        }
+        ctx.addToDo(input.value)
     }
 
     return (
-        <input id="input-todo" ref={input_ref} type="text" placeholder="Enter a new task" onKeyDown={e=> {
-            if(e.key === 'Enter'){
+        <input id="input-todo" ref={input_ref} type="text" placeholder="Enter a new task" onKeyDown={e => {
+            if (e.key === 'Enter') {
                 save()
             }
-        }}/>
+        }} />
     )
 }
