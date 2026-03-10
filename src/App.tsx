11e7a562fafc877cdd6404595a14f35587c9fcfa
elisheva1885 <elisheva1885@gmail.com>
2026-03-10 11:36:34 +0200

    complete button changes color just on hover

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 
 import './App.css'
 import { AddToDo } from './components/add-todo'
+import { ToDoItem } from './components/todo-item'
 import { ToDosProvider } from './store/todos-context'
+import type { ToDo } from './store/types'
 
 const App = () => {
-  
+  const todo:ToDo = {
+    id:1,
+    description: 'aaaaaaaaaaaaaaaa',
+    status:'complete'
+  }
   
   return (
     <ToDosProvider>
+      <ToDoItem todo={todo}/>
       <AddToDo/>
+
     </ToDosProvider>
   )
 }
